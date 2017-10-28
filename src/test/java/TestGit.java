20d7f6dabccb035df9af2a0d9bf153aa4d1d2757
Dmitrii Filev <33070640+filev@users.noreply.github.com>
2017-10-28 13:47:34 -0700

    new changings

--- src/test/java/TestGit.java
+++ src/test/java/TestGit.java
@@ -2,5 +2,6 @@ public class TestGit {
     public static  void main(String[]args){
         System.out.println("Hello Git");
         System.out.println("Changings");
+        System.out.println("One more");
     }
 }
